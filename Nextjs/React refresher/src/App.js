6cd23ae140864cd9fddfdf119f8028de47483c6c
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2021-09-08 22:09:29 +0900

    Add features
    - Add Routing
    - Add Links and Navigation
    - Scoping components styles with CSS modules
    - Outputting list of data and components

--- Nextjs/React refresher/src/App.js
+++ Nextjs/React refresher/src/App.js
@@ -1,7 +1,27 @@
+import { Route, Switch } from "react-router-dom";
 
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/Layout/MainNavigation";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup" exact>
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ Nextjs/React refresher/src/components/Layout/MainNavigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">All Meetups</NavLink>
+          </li>
+          <li>
+            <NavLink to="/new-meetup">Add New Meetup</NavLink>
+          </li>
+          <li>
+            <NavLink to="/favorites">My Favorites</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
